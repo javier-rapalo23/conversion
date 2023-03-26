df15d2ae1afa22b9e506cc57439520ad25bd2937
javier-rapalo23 <javierorellana2015.jaor@gmail.com>
2023-03-26 00:14:00 -0600

    25-marzo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App= () => {
+  const valor = 'triste'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit ssS<code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='box'>
+      
+      <p className='intro'>Conversion de Metros a Yardas</p>
     </div>
+    
   );
 }
 
